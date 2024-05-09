8c1bf213013077638bf9468d46de1a19300519fd
Revolver_DX <114694204+RvlDX@users.noreply.github.com>
2024-05-09 18:25:07 +0700

    Add files via upload

---
+++ ProjectTiketBus.java
@@ -0,0 +1,25 @@
+package projecttiketbus;
+
+import javax.swing.JFrame;
+
+public class ProjectTiketBus {
+    public static void main(String[] args) {
+        // Membuat instance JFrame
+        JFrame frame = new JFrame("Program Pembelian Tiket Bus");
+        
+        // Membuat instance panel penjualan tiket bus
+        PenjualanTiketBus panel = new PenjualanTiketBus();
+        
+        // Menambahkan panel ke frame
+        frame.getContentPane().add(panel);
+        
+        // Atur ukuran frame
+        frame.setSize(615, 550);
+        
+        // Menentukan operasi default saat menutup frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Menampilkan frame
+        frame.setVisible(true);
+    }
+}
